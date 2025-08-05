cbc9ba9711d6e9cd0a9091a640a1bc5f7a55f6a2
Shin <sirapop1998@gmail.com>
2025-08-05 14:08:53 +0700

    feat: add answer to question-3

--- question-3.js
+++ question-3.js
@@ -1,1 +1,21 @@
 // เริ่มเขียนโค้ดตรงนี้
+
+async function getUsers() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    return data;
+}
+
+async function filterDataByUserLength(length) {
+    try{
+        const userData = await getUsers()
+        const userFilterLength = userData.filter((user) => user.name.length > length);
+        const userNameFilter = userFilterLength.map((user)=> user.name);
+        return console.log(userNameFilter);
+    }catch (error) {
+        console.log(error);
+    }
+    
+}
+
+filterDataByUserLength(17);
